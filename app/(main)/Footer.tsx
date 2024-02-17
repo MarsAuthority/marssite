7a093006f8209b50f19daa100f99729dbd545e81
MarsAuthority <chinamarsmail@gmail.com>
2024-02-17 21:05:03 +0800

    add

--- app/(main)/Footer.tsx
+++ app/(main)/Footer.tsx
@@ -3,7 +3,6 @@ import Link from 'next/link'
 import React from 'react'
 
 import { CursorClickIcon, UsersIcon } from '~/assets'
-import { PeekabooLink } from '~/components/links/PeekabooLink'
 import { Container } from '~/components/ui/Container'
 import { kvKeys } from '~/config/kv'
 import { navigationItems } from '~/config/nav'
